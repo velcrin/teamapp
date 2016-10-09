a2f86de98492bc9b14f55fcb5ded2d52ecca0c94
Vincent <vincent.elcrin@gmail.com>
2016-10-09 23:44:34 +0200

    Add some of the missing pages

--- public/js/home.controller.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-  function HomeController() {
-    this.sport = 'football';
-  }
-
-  angular.module('teamapp').controller('HomeController', HomeController);
-})();
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname + '/public'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/events', (req, res) => res.render('events'));
+app.get('/events/:id', (req, res) => res.render('edit', { id: req.params.id }));
+app.get('/events/:id/share', (req, res) => res.render('share', { id: req.params.id }));
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
